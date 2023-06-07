29340b1675ab5487a88c96ea38ed825591563a33
satzbe <satzb.e.17@gmail.com>
2023-06-07 18:50:01 +0530

    hover css added

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Card = ({ id, url, name }: cardProps) => {
     return (
-        <div className="bg-white rounded shadow p-4">
-            <img src={url} alt={id} className='w-full h-60' />
+        <div className="bg-white shadow p-4 rounded-md hover:scale-110">
+            <img src={url} alt={id} className='w-full h-60 rounded-md ' />
             <h3 className="text-lg font-bold mb-2">{name}</h3>
         </div>
     )
