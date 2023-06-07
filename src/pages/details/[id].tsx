0f24b7a1126ab00a59fb02048a94b24ea4f7d1c3
satzbe <satzb.e.17@gmail.com>
2023-06-07 18:33:46 +0530

    component name uppdated

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 
-const index = ({ datas }: appProps) => {
+const Details = ({ datas }: appProps) => {
     const { download_url, author } = datas[0]
     const backSvg = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
@@ -31,7 +31,7 @@ const index = ({ datas }: appProps) => {
     )
 }
 
-export default index
+export default Details
 
 // This gets called on every request
 export async function getServerSideProps(ctx: any) {
